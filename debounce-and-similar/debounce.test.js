d9191f262b7debf0b45987024da4b4fd036611a4
agBeta <alirezaghorbanzadeh@yahoo.com>
2024-01-13 11:53:29 +0330

    incorrect implementation works not sure why. (read description)
    The incorrect implementation that does not bind this or uses arrow function still
    works. Even if you choose incorrect_2 the last test fails, which means inccorect_2
    is NOT actually incorrect, but correct.
    On the other hand, if you remove .bind(obj) from second test case, even correct
    implementation will fail.
    The explanation by algoexpert insists on not using arrow function. Not sure why.

--- debounce-and-similar/debounce.test.js
+++ debounce-and-similar/debounce.test.js
@@ -21,99 +21,89 @@ test("debounce (practice)", { concurrency: false }, async () => {
 
 
     // For await see https://nodejs.org/api/test.html#extraneous-asynchronous-activity.
-    // await test("correct debounced function works", async (t) => {
-    //     let value = 0;
-    //     let cntCall = 0;
-    //     const f = function (num = 1) {
-    //         // t.diagnostic(`f1 (${num}) -- at ${Date.now()}`);
-    //         value += num;
-    //         cntCall++;
-    //     }
-
-    //     const fDebounced = _makeDebounce_(f, delay, "correct");
-    //     fDebounced(4);
-    //     // f shouldn't be called until delay:
-    //     assert.strictEqual(cntCall, 0);
-    //     assert.strictEqual(value, 0);
-
-    //     await justWait(delay + epsilon); // <-- bad practice but ok.
-
-    //     // Now delay has passed, so f should be executed by now:
-    //     assert.strictEqual(cntCall, 1);
-    //     assert.strictEqual(value, 4);
-
-    //     fDebounced(8);
-    //     // f shouldn't be called until delay:
-    //     assert.strictEqual(value, 4);
-    //     assert.strictEqual(cntCall, 1);
-    //     // We call again to reset the waiting interval.
-    //     await justWait(delay / 2);
-    //     fDebounced(16);
-
-    //     assert.strictEqual(cntCall, 1);
-    //     assert.strictEqual(value, 4);
-
-    //     await justWait(delay / 2 + epsilon);
-    //     //  The call(16) was made less than delay ms ago. So even though the call(8) was made
-    //     //  more than delay ms ago, but the waiting interval has been reset by call(16). So ...
-    //     assert.strictEqual(cntCall, 1);
-    //     assert.strictEqual(value, 4);
-
-    //     await justWait(delay + epsilon);
-
-    //     // The call(8) should be ignored (i.e. it's timeout should be cleared by call(16) ).
-    //     assert.strictEqual(cntCall, 2);
-    //     assert.strictEqual(value, 16 + 4);
-    // });
-
-
-    // await test("correct debounce function gracefully handles 'this' binding", async () => {
-    //     let cntCall = 0;
-    //     const obj = {
-    //         value: 0,
-    //         f: function (num = 1) {
-    //             this.value += num;
-    //             cntCall++;
-    //         }
-    //     }
-
-    //     // First let's have a sanity check.
-    //     obj.f(5);
-    //     assert.strictEqual(obj.value, 5);
-    //     obj.f(-5);
-    //     assert.strictEqual(obj.value, 0);
-    //     assert.strictEqual(cntCall, 2);
-    //     // Now reset cntCall for actual assertions
-    //     cntCall = 0;
-
-
-    //     // 🧯 Don't forget to bind!
-    //     const fDebounced = _makeDebounce_(obj.f.bind(obj), delay, "correct");
-    //     fDebounced(4);
-    //     await justWait(delay + epsilon);
-
-    //     assert.strictEqual(cntCall, 1);
-    //     assert.strictEqual(obj.value, 4);
-    // });
-
-
-    await test("incorrect 1 debounce doesn't work", async () => {
+    await test("correct debounced function works", async (t) => {
+        let value = 0;
         let cntCall = 0;
-        let cntThrow = 0;
+        const f = function (num = 1) {
+            // t.diagnostic(`f1 (${num}) -- at ${Date.now()}`);
+            value += num;
+            cntCall++;
+        }
+
+        const fDebounced = _makeDebounce_(f, delay, "correct");
+        fDebounced(4);
+        // f shouldn't be called until delay:
+        assert.strictEqual(cntCall, 0);
+        assert.strictEqual(value, 0);
+
+        await justWait(delay + epsilon); // <-- bad practice but ok.
+
+        // Now delay has passed, so f should be executed by now:
+        assert.strictEqual(cntCall, 1);
+        assert.strictEqual(value, 4);
+
+        fDebounced(8);
+        // f shouldn't be called until delay:
+        assert.strictEqual(value, 4);
+        assert.strictEqual(cntCall, 1);
+        // We call again to reset the waiting interval.
+        await justWait(delay / 2);
+        fDebounced(16);
+
+        assert.strictEqual(cntCall, 1);
+        assert.strictEqual(value, 4);
+
+        await justWait(delay / 2 + epsilon);
+        //  The call(16) was made less than delay ms ago. So even though the call(8) was made
+        //  more than delay ms ago, but the waiting interval has been reset by call(16). So ...
+        assert.strictEqual(cntCall, 1);
+        assert.strictEqual(value, 4);
+
+        await justWait(delay + epsilon);
+
+        // The call(8) should be ignored (i.e. it's timeout should be cleared by call(16) ).
+        assert.strictEqual(cntCall, 2);
+        assert.strictEqual(value, 16 + 4);
+    });
+
+
+    await test("correct debounce function gracefully handles 'this' binding", async () => {
+        let cntCall = 0;
         const obj = {
             value: 0,
             f: function (num = 1) {
                 this.value += num;
                 cntCall++;
-                // try {
-                //     console.log(" ---- ", this);
-                // } catch (e) {
-                //     cntThrow++;
-                //     //  Why try/catch? See few lines below.
-                //     //  It's difficult to propagate back the error inside setTimeout.
-                //     //  But we must catch the error, so that the test case doesn't fail.
-                //     //  For assertion, we'll check obj.value.
-                // }
+            }
+        }
+
+        // First let's have a sanity check.
+        obj.f(5);
+        assert.strictEqual(obj.value, 5);
+        obj.f(-5);
+        assert.strictEqual(obj.value, 0);
+        assert.strictEqual(cntCall, 2);
+        // Now reset cntCall for actual assertions
+        cntCall = 0;
+
+
+        // 🧯 Don't forget to bind!
+        const fDebounced = _makeDebounce_(obj.f.bind(obj), delay, "correct");
+        fDebounced(4);
+        await justWait(delay + epsilon);
+
+        assert.strictEqual(cntCall, 1);
+        assert.strictEqual(obj.value, 4);
+    });
+
+
+    await test("incorrect 1 debounce doesn't work", async () => {
+        let cntCall = 0;
+        const obj = {
+            value: 0,
+            f: function (num = 1) {
+                cntCall++;
+                this.value += num;
             }
         }
 
