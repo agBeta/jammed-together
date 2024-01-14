3ee922cbb9d0e4012bace900b69423529695a62a
agBeta <alirezaghorbanzadeh@yahoo.com>
2024-01-14 14:47:56 +0330

    add more to experiment-01

--- promise/experiment-01.js
+++ promise/experiment-01.js
@@ -31,6 +31,24 @@ function f3(){
     //? prints 2 -> 4 -> 1 -> 3.  🔷
 }
 
+function f3_when_busy(){
+    setTimeout(() => console.log("1"), 4.7);
+    setTimeout(() => console.log("2"), 1);
+    setTimeout(() => console.log("3"), 4);
+    setTimeout(() => console.log("4"), 0);
+    let sum = 0;
+    //  You can change number below based on your hardware. The point is to make sure 'for' below takes
+    //  more than a few milliseconds.
+    for (let i = 0; i < 2_000_000_000; i++) {
+        sum += i;
+    }
+    // Let's console.log sum to assure that no optimization happens behind the scenes.
+    console.log(sum);
+
+    // prints: the same result as f3().
+}
+
 // f1();
 // f2();
-f3();
+// f3();
+f3_when_busy();
