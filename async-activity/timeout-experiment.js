0a93eabf9112c8f7088606f7d205d05f04aee4e4
agBeta <alirezaghorbanzadeh@yahoo.com>
2024-01-26 09:36:14 +0330

    add timeout experiment

---
+++ async-activity/timeout-experiment.js
@@ -0,0 +1,29 @@
+function doSomethingHeavy(){
+    const n = Math.floor(Math.random() * 1_000_000) + 100_000_000;
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+    return sum;
+}
+
+for (let i = 1; i <= 100; i++) {
+    setTimeout(() => {
+        doSomethingHeavy();
+        console.log(`Finished heavy job: ${i}`);
+        process.nextTick(() => {
+            console.log(`executing nextTick callback of ${i}`);
+        });
+    }, 0);
+}
+
+
+/* prints
+    Finished heavy job: 1
+    executing nextTick callback of 1
+    Finished heavy job: 2
+    executing nextTick callback of 2
+    Finished heavy job: 3
+    executing nextTick callback of 3
+    ...
+*/
