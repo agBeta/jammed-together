5b4c132a1baf8d5307c0a070f72cc0b0d3838a3b
agBeta <alirezaghorbanzadeh@yahoo.com>
2024-01-26 09:01:18 +0330

    update bind, add empty test file

--- bind/bind.js
+++ bind/bind.js
@@ -1,13 +1,13 @@
 // @ts-ignore
 Function.prototype.ourBind = function(thisContext, ...args) {
-    //  We need to function to thisContext, so that all "this" keywords inside the function
+    //  We need to bind function to thisContext, so that all "this" keywords inside the function
     //  will automatically be bound to thisContext.
 
     const fnThatWeAreCurrentlyTryingToBind = this; // Why? We are writing this code in prototype.
     
     return function (...argsOfBoundFunction) {
 
-        //  Don't do this
+        //  Don't do this:
         //  ->  thisContext.fn = fnThatWeAreCurrentlyTryingToBind;
         //  If function (function we are currently on and trying to bind), has a for(in) loop inside its
         //  implementation, then fn would appear in the loop, which isn't what we want.
@@ -17,7 +17,7 @@ Function.prototype.ourBind = function(thisContext, ...args) {
         thisContext[symbol] = fnThatWeAreCurrentlyTryingToBind;
 
         const ret = thisContext[symbol](...args, ...argsOfBoundFunction);
-        //Now we reach here, execution context of ret is already created. So we can safely:
+        //  Now we reach here, execution context of ret is already created. So we can safely:
         delete thisContext[symbol];
         return ret;
     };
