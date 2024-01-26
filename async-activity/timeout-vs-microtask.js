b7c735b4ab7f259062ff29c0a981259eec62d311
agBeta <alirezaghorbanzadeh@yahoo.com>
2024-01-26 09:47:20 +0330

    update timeout-vs-microtask to more sophisticated experiment

--- async-activity/timeout-vs-microtask.js
+++ async-activity/timeout-vs-microtask.js
@@ -1,4 +1,4 @@
-function doSomethingHeavy(){
+function doSomethingHeavy() {
     const n = Math.floor(Math.random() * 1_000_000) + 100_000_000;
     let sum = 0;
     for (let i = 1; i <= n; i++) {
@@ -7,12 +7,14 @@ function doSomethingHeavy(){
     return sum;
 }
 
-for (let i = 1; i <= 100; i++) {
+for (let i = 1; i <= 100 /*maybe 10 is better*/; i++) {
     queueMicrotask(() => {
         doSomethingHeavy();
         console.log(`Finished heavy job: ${i}`);
         process.nextTick(() => {
             console.log(`executing nextTick callback of ${i}`);
+            setTimeout(() => console.log(`inner setTimeout callback of ${i}`), 0);
+            process.nextTick(() => console.log(`inner nextTick callback of ${i}`));
         });
     });
 }
@@ -30,4 +32,12 @@ for (let i = 1; i <= 100; i++) {
     executing nextTick callback of 2
     ...
     executing nextTick callback of 100
+    inner nextTick callback of 1
+    inner nextTick callback of 2
+    ...
+    inner nextTick callback of 100
+    inner setTimeout callback of 1
+    inner setTimeout callback of 2
+    ...
+    inner setTimeout callback of 100
 */
